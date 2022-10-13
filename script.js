d4a6edbb0a2c4e2986c728c4a330c114f30da6a9
ZacharyNWilliams <zacharynwilliams9@gmail.com>
2022-10-12 19:37:49 -0700

    Refresher

---
+++ script.js
@@ -0,0 +1,29 @@
+let firstName = "Zachary ";
+
+let lastName = "Williams";
+
+let fullName = firstName + lastName;
+
+console.log(fullName);
+
+let population = 672351;
+
+
+console.log(population);
+
+population = population * 3;
+console.log(population);
+
+
+let javascriptIsCool = true;
+
+let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+
+
+for(let i = 1; i <6; i++){
+    console.log(i)
+    }
+
+    for(let i = 0; i <5; i++){
+        console.log(i)
+        }
